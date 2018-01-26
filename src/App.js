93a5637c225e517d599645b2e8e61d4c54f16e35
Leonardo Frangelli <leonardo.frangelli@raisin.com>
2018-01-26 10:27:33 +0100

    Creating repository

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import {
+    BrowserRouter as Router,
+    Route
+} from 'react-router-dom';
+import Home from './components/Home';
+
+import './App.css';
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Route exact path="/" component={Home}/>
+      </Router>
+    );
+  }
+}
+
+export default App;
