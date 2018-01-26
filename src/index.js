6a0f2324165c4c74ac10fbabf5bd7aa9d6e4d768
Dominic Kolbe <dominic.kolbe@raisin.com>
2018-01-26 18:31:07 +0100

    changed color

--- src/index.js
+++ src/index.js
@@ -4,14 +4,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import {indigo500} from 'material-ui/styles/colors';
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import store from './store';
 
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: indigo500,
+  },
+});
+
 ReactDOM.render(
-  <MuiThemeProvider>
+  <MuiThemeProvider muiTheme={muiTheme}>
     <Provider store={store}>
       <App />
     </Provider>
